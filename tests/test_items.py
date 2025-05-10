a194066b7aba7d417ab475271bc8e6d5216a9171
Audrey <audreynguepndjio@email.com>
2025-05-10 18:59:46 +0200

    fix: ajout du sys.path pour import main sur GitHub Actions

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,9 +1,9 @@
 import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from fastapi.testclient import TestClient
 from main import app
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 client = TestClient(app)
 
