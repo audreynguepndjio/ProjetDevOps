7f458f8e0d0d1c33ba76fb5905c8977bbaf541c1
Audrey <audreynguepndjio@email.com>
2025-05-10 18:54:55 +0200

    fix: flake8 import position

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,11 +1,9 @@
-from fastapi.testclient import TestClient
 import sys
 import os
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+from fastapi.testclient import TestClient
 from main import app
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 client = TestClient(app)
 
