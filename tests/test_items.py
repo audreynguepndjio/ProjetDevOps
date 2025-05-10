bf7fd40128e4f41f4479616aed3cb433dc5f5f9b
Audrey <audreynguepndjio@email.com>
2025-05-10 19:34:06 +0200

    fix: dernier correctif flake8 E402/W291

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,10 +1,9 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from fastapi.testclient import TestClient
 from main import app
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 client = TestClient(app)
 
